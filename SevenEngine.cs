423b5d2379e6bb63234a386b4fb039477d79ea2d
nessd10 <nessd10@yahoo.com>
2012-11-09 23:45:25 -0500

    Work please

--- SevenEngine.cs
+++ SevenEngine.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+/*
+ * Current Problems with the Seven Physics Engine
+ * -Going foward when not colliding with a Collidable Object
+ * -Cannot jump if there is a collidable object above the player
+ *          *doesn't seem to take into account the x value of the player and object
+ * -Animation bug, keep walking while running into wall, but if user lets go of 'D' 
+ *                  then animation will stop looping if they press D again
+ * -Comments :), I was just a little confused if rightCollision and aboveCollision are in perspective
+ *               to the player or the object colliding with the player
+ * 
+ * I know all of these are pretty easy fixes just thought listing them would help us work through it
+ * 
+ * Also, GRATZ MAKING IT THROUGH WORK!!!!
+ */
+
 namespace PlatformerProject
 {
     class SevenEngine
@@ -36,10 +51,6 @@ namespace PlatformerProject
             }
             return false;
         }
-
-        
-
-
 
     }
 }
